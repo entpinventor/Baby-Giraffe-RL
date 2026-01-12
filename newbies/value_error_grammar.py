e401609a2a132dcc17c413ee2ee8958ceaa5666c
summer <dennis46135@gmail.com>
2026-01-12 19:57:26 +0900

    architecture setting

---
+++ newbies/value_error_grammar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  9 16:15:00 2026
+
+@author: summer
+"""
+"""
+class MyClass:
+    pass
+
+obj = MyClass()
+
+if not isinstance(obj, MyClass):
+    raise ValueError("MyClass 객체가 아닙니다!")
+    
+"""
+
+import torch
---
+++ tests/test_math.py
@@ -0,0 +1,7 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    print('asfdgasgasg')
+    assert 5 - 3 == 2
+
